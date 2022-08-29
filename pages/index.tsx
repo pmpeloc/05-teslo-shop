3ffccfa2906623a778133cea4c52a8af9521f310
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-08-29 12:34:48 -0300

    feat: Layout added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 
 const Home: NextPage = () => {
-  return <h1>Teslo Shop</h1>;
+  return (
+    <ShopLayout
+      title={'Teslo Shop - Home'}
+      pageDescription={'Encuentra los mejores productos de Teslo'}>
+      <Typography variant='h1' component='h1'>
+        Tienda
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
