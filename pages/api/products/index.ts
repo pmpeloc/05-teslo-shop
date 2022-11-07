c5c234dec3fcf792335a64060a17249ae0673273
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-07 17:24:29 -0300

    feat: Filter to Get Products added

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { db } from '../../../database';
+import { db, SHOP_CONSTANTS } from '../../../database';
 import { Product } from '../../../models';
 import { IProduct } from '../../../interfaces';
 
@@ -19,8 +19,13 @@ export default function handler(
 }
 
 const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { gender = 'all' } = req.query;
+  let condition = {};
+  if (gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+    condition = { gender };
+  }
   await db.connect();
-  const products = await Product.find()
+  const products = await Product.find(condition)
     .select('title images price inStock slug -_id')
     .lean();
   await db.disconnect();
