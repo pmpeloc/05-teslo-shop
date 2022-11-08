c6f10d1601f2526efe3819856af14902bdc58e22
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-07 21:02:50 -0300

    feat: Search product added

--- models/Product.ts
+++ models/Product.ts
@@ -37,6 +37,8 @@ const productSchema = new Schema(
   { timestamps: true }
 );
 
+productSchema.index({ title: 'text', tags: 'text' });
+
 const Product: Model<IProduct> =
   mongoose.models.Product || model('Product', productSchema);
 
