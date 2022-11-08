233782a0232175baf2fe69f27314583cadbd3bf8
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-07 21:21:07 -0300

    feat: SWR hook added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
-import { initialData } from '../database/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) =>
+  fetch(...args).then((res) => res.json());
+
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher);
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  console.log(data);
   return (
     <ShopLayout
       title={'Teslo Shop - Home'}
@@ -15,9 +23,9 @@ const Home: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         Todos los productos
       </Typography>
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
